f87ffdf2975eef242394a2495ebe962010dd168d
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-14 20:36:19 +0100

    Updates

---
+++ precache-manifest.55f2b00b385c372a262746df1c729daa.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d384919442a93c3b69fc1f4bd53293ca",
+    "url": "/WayneKopman-module-ii/index.html"
+  },
+  {
+    "revision": "c5d7f7994aaa362c02e0",
+    "url": "/WayneKopman-module-ii/static/css/main.1327cf45.chunk.css"
+  },
+  {
+    "revision": "2a3606e985bf067570b3",
+    "url": "/WayneKopman-module-ii/static/js/2.916ec0fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WayneKopman-module-ii/static/js/2.916ec0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d7f7994aaa362c02e0",
+    "url": "/WayneKopman-module-ii/static/js/main.cc86b937.chunk.js"
+  },
+  {
+    "revision": "54d1a13acb6f46e661a6",
+    "url": "/WayneKopman-module-ii/static/js/runtime-main.0ab055a9.js"
+  },
+  {
+    "revision": "09ac7095a22beadb2744304e6dd4971c",
+    "url": "/WayneKopman-module-ii/static/media/About-Hero-Bean-Bush.09ac7095.jpg"
+  },
+  {
+    "revision": "d4b448ce262e2adda8514e8d8b570562",
+    "url": "/WayneKopman-module-ii/static/media/About-us.d4b448ce.png"
+  },
+  {
+    "revision": "bd86e86222ddee212804e20928f66314",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-Brazil.bd86e862.png"
+  },
+  {
+    "revision": "5ed1508cf02550913f923eb3b95c23bc",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-Colombia.5ed1508c.png"
+  },
+  {
+    "revision": "e86334b0c740929d293aebfdf0aa3139",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-Ecuador.e86334b0.png"
+  },
+  {
+    "revision": "ae542c23900e42f0c017b7e62c70ec78",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-India.ae542c23.png"
+  },
+  {
+    "revision": "43a4a4c176f472d58e35ab4ca4f91397",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-Kenya.43a4a4c1.png"
+  },
+  {
+    "revision": "e12cac091da817826ebbb51322476c04",
+    "url": "/WayneKopman-module-ii/static/media/Coffee-Bags-Flags-Peru.e12cac09.png"
+  },
+  {
+    "revision": "6cefa761a1edc4fe7bf951539190cfe9",
+    "url": "/WayneKopman-module-ii/static/media/No-Cofee-mug.6cefa761.jpg"
+  },
+  {
+    "revision": "10e65949346ee7983ea245f88217afa1",
+    "url": "/WayneKopman-module-ii/static/media/SocietyCoffee-Logo-Web.10e65949.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/WayneKopman-module-ii/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "2c42a0ea06fdbf3a0532a61242ac7598",
+    "url": "/WayneKopman-module-ii/static/media/great-coffee-bean-Society-Coffee.2c42a0ea.jpg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/WayneKopman-module-ii/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/WayneKopman-module-ii/static/media/times-solid.2c5552a1.svg"
+  }
+]);
