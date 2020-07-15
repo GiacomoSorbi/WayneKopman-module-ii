24b77b49529d83fbda403d1463e5cf2e9847de39
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-15 14:26:27 +0100

    Convert class component to functional

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 //Browser router keeps UI in sync with URL
 import { BrowserRouter as Router } from "react-router-dom";
 import "./app.css";
-import { Route } from "react-router-dom";
 
 //importing the structural components of the website in the main App
 import Header from "./components/Header";
@@ -14,20 +13,18 @@ import { DataProvider } from "./components/Context";
 
 //This function exports the various components that make up the website. This includes the Header, section or body and the Footer
 
-class App extends React.Component {
-  render() {
-    return (
-      <DataProvider>
-        <Router basename={process.env.PUBLIC_URL}>
-          <Header />
-          <div className="app">
-            <Section />
-          </div>
-          <Footer />
-        </Router>
-      </DataProvider>
-    );
-  }
+function App(props) {
+  return (
+    <DataProvider>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Header />
+        <div className="app">
+          <Section />
+        </div>
+        <Footer />
+      </Router>
+    </DataProvider>
+  );
 }
 
 export default App;
