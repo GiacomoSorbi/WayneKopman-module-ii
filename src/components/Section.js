604a8466cdfdc21815441cb723f00cfd63314fb6
Wayne Kopman <56189380+WayneKopman@users.noreply.github.com>
2020-07-15 11:42:33 +0100

    Added switch and other changes

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Products from "./section/Products";
 import Details from "./section/Details";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Cart from "./section/Cart";
 import Payment from "./section/Payment";
 import Homepage from "./section/Homepage";
@@ -10,12 +10,14 @@ import About from "./section/About";
 function Section(props) {
   return (
     <section>
-      <Route path="/" component={Homepage} exact />
-      <Route path="/product" component={Products} exact />
-      <Route path="/product/:id" component={Details} />
-      <Route path="/cart" component={Cart} />
-      <Route path="/payment" component={Payment} />
-      <Route path="/about" component={About} />
+      <Switch>
+        <Route path="/" component={Homepage} exact />
+        <Route path="/product" component={Products} exact />
+        <Route path="/product/:id" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/payment" component={Payment} />
+        <Route path="/about" component={About} />
+      </Switch>
     </section>
   );
 }
